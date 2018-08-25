efbd66f97e14c690f6d1f3ff551912c8151c99b5
Michael Fung <hkuser2001@gmail.com>
2018-08-25 14:21:02 +0800

    add perf tuning: use RAM for QoS 1/2

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Documentation for building a VerneMQ docker image.
 
 It is based on the offical [docker-vernemq](https://github.com/erlio/docker-vernemq) Dockerfile. But the base distro used here is Ubuntu instead.
+
+
+## Performance tuning
+
+### Disable levelDB
+
+Set environment `VMQ_USE_RAM` to 'y'.
+
+Ref: [Subscriber message delivery performance](https://github.com/erlio/vernemq/issues/771)
--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,39 @@ CMD=$1
 # set env params:
 # export MYEVN
 
+
+#
+# disable leveldb to speed up perf when using QoS 1/2
+# credit: https://github.com/chriswue
+#
+disable_leveldb() {
+    cd /opt/vernemq/bin
+    while [ : ]; do
+        ./vmq-admin metrics show > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "found running vernemq, disabling leveldb ..."
+            ./vmq-admin plugin disable -m vmq_lvldb_store -f msg_store_write -a 2
+            ./vmq-admin plugin disable -m vmq_lvldb_store -f msg_store_find -a 1
+            ./vmq-admin plugin disable -m vmq_lvldb_store -f msg_store_read -a 2
+            ./vmq-admin plugin disable -m vmq_lvldb_store -f msg_store_delete -a 2
+            echo "... leveldb disabled"
+            exit 0
+        fi
+        echo "vernemq not yet running, waiting a few secs ..."
+        sleep 3
+    done
+}
+
+if [ -n ${VMQ_USE_RAM} ] ;
+    disable_leveldb &
+fi
+
 case "$CMD" in
   "start" )
     cd /opt/vernemq
     /usr/local/bin/su-exec nobody:nogroup ./bin/vernemq console -noshell -noinput
     ;;
+
 
    * )
     # Run custom command. Thanks to this line we can still use
